a796e8a27e0c6066bf14016eef3f5397364a5474
Yash <yash.virmani@technologies33.com>
2022-02-06 17:27:02 +0530

    final commit

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/products/:productId/reviews', async (req, res) => {
         const response = await request(`${generateScrapeUrl(api_Key)}&url=https://www.amazon.co.in/product-review/${productId}`)
         res.json(JSON.parse(response));
     } catch (error) {
-
+        res.json(error);
     }
 })
 //GET PRODUCT OFFERS
@@ -46,7 +46,7 @@ app.get('/products/:productId/offers', async (req, res) => {
         const response = await request(`${generateScrapeUrl(api_Key)}&url=https://www.amazon.co.in/gp/offer-listing/${productId}`)
         res.json(JSON.parse(response));
     } catch (error) {
-
+        res.json(error);
     }
 })
 //GET SEARCH RESULT
@@ -54,10 +54,11 @@ app.get('/search/:searchQuery', async (req, res) => {
     const { searchQuery } = req.params;
     const { api_Key } = req.query;
     try {
-        const response = await request(`${generateScrapeUrl(api_Key)}&url=https://www.amazon.co.in/s?k=${searchQuery}`)
-        res.json(JSON.parse(response.results));
+        let response = await request(`${generateScrapeUrl(api_Key)}&url=https://www.amazon.co.in/s?k=${searchQuery}`)
+        const { results } = JSON.parse(response);
+        res.json(results);
     } catch (error) {
-
+        res.json(error);
     }
 })
 
